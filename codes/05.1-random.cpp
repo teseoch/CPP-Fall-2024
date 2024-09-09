3d1a7e0f361f48419bf7770a1c668e223d4d3b20
teseoch <teseo.schneider@gmail.com>
2024-09-09 12:49:10 -0700

    random

---
+++ codes/05.1-random.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <random>
+#include <cstdlib>
+
+int main()
+{
+    //Task 1: create a random number between 2 and 3
+
+    //Task 2: create a random number between 2 and 3 using C++11 random library
+
+    /////////////////////Approximate PI////////////////////////
+
+    //Task 3: Create a variable to store the trials and one to create the hits.
+
+    //Task 4: Randomly generate points in the square from -1 to 1.
+    //        If the point is inside the circle, increment the hits.
+    //        Compute the ratio of hits to trials and multiply by 4 to get an approximation of PI.
+
+    return 0;
+}
