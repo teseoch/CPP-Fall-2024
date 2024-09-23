0f573fd5ce91f71b4c9b3ec8230f70e4aac8f57a
teseoch <teseo.schneider@gmail.com>
2024-09-23 12:12:04 -0700

    new code

---
+++ codes/23-auto_tracing.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+int f(int x, int y)
+{
+    x = 10;
+    y = 6;
+    return x + y;
+}
+
+float g(int a, int b)
+{
+    return a + b;
+}
+
+int h(float a, float b)
+{
+    return a / b;
+}
+
+int main()
+{
+    int x{111};
+    int y{116};
+    auto z{f(x, y)};
+
+    std::cout << "Begin: " << x << " " << y << " " << z << std::endl;
+
+    z = g(2.5, 2.5);
+
+    if (z == 4)
+    {
+        int x{0};
+        x = 200;
+        y = 1000;
+        float z = h(x, y);
+    }
+
+    std::cout << "End: " << x << " " << y << " " << z << std::endl;
+
+    return 0;
+}
