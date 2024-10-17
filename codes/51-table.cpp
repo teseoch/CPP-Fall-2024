50c0bfa74ef8775d2e6b0afbc15f76b47f79b693
teseoch <teseo.schneider@gmail.com>
2024-10-17 12:56:01 -0700

    new files

---
+++ codes/51-table.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    //Create table T of integer values with 3 rows and 5 columns.
+    //Notice that the elements in the initializer for T
+    //are initializers for one-dimensional vectors.
+    //Note that "jagged" tables are possible (rows are not required to have the same size)
+    std::vector<std::vector<int>> T{
+        {1, 2, 3, 4, 5},
+        {6, 7, 8, 9, 10},
+        {11, 12, 13, 14, 15}};
+
+    //Print the table with for-each loops
+    // TO DO
+
+    std::cout << std::endl;
+
+    //Print the table with a traditional indexing loop
+    // TO DO
+
+    return 0;
+}
---
+++ codes/53-pair.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <utility>
+#include <string>
+
+int main()
+{
+    int a{5};
+    std::string b{"Hello"};
+
+    return 0;
+}
